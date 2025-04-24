7da5bf9669d4df8c496c1d229eb4dbd83a47b464
leovsch <leo.25.1996@gmail.com>
2025-04-24 15:03:43 +0200

    update file names and add renaming of files

--- code_gen.py
+++ code_gen.py
@@ -1,8 +1,23 @@
 
 from dots_infrastructure.code_gen.code_gen import CodeGenerator
+import json
+import os
 
-code_generator = CodeGenerator("output")
+code_generator = CodeGenerator()
 with open("input.json", "r") as input_file:
     input_data = input_file.read()
 
-code_generator.code_gen(input=input_data, code_output_dir="src", documentation_ouput_dir="documentation")
+json.loads(input_data)
+cs_name = input_data["name"]
+cs_python_name = code_generator.get_python_filename(cs_name)
+
+if os.path.exists("src/ExampleCalculationService"):
+    os.rename("src/ExampleCalculationService", f"src/{cs_name}")
+
+if os.path.exists(f"src/{cs_name}/calculation_service_test.py"):
+    os.rename(f"src/{cs_name}/calculation_service_test.py", f"src/{cs_name}/{cs_python_name}.py")
+
+if os.path.exists("test/test_template.py"):
+    os.rename("test/test_template.py", f"test/test_{cs_python_name}.py")
+
+code_generator.code_gen(input=input_data, code_output_dir=f"{cs_name}/src", documentation_ouput_dir="docs")
