137731504c611e6f8b963bcaab2b08cc2e80a519
leovsch <leo.25.1996@gmail.com>
2025-04-29 10:03:00 +0200

    update code gen script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
-FROM python:3.9.0
-# If needed you can use the official python image (larger memory size)
-#FROM python:3.9.0
+FROM python:3.13
 
 RUN mkdir /app/
 WORKDIR /app
--- code_gen.py
+++ code_gen.py
@@ -1,14 +1,22 @@
 
+from typing import List
 from dots_infrastructure.code_gen.code_gen import CodeGenerator
 import json
 import os
+from dataclasses import dataclass
 
-def replace_string_in_file(file_path, old_string, new_string):
+@dataclass
+class FindReplace:
+    find : str
+    replace : str
+
+def replace_string_in_file(file_path, find_replace : List[FindReplace]):
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             filedata = file.read()
 
-        filedata = filedata.replace(old_string, new_string)
+        for item in find_replace:
+            filedata = filedata.replace(item.find, item.replace)
 
         with open(file_path, 'w') as file:
             file.write(filedata)
@@ -20,6 +28,7 @@ with open("input.json", "r") as input_file:
 parsed_input_data = json.loads(input_data)
 cs_name = code_generator.camel_case(parsed_input_data["name"])
 cs_python_name = code_generator.get_python_filename(cs_name)
+cs_python_base_class_name = code_generator.get_base_class_name(cs_name)
 
 if os.path.exists("src/ExampleCalculationService"):
     os.rename("src/ExampleCalculationService", f"src/{cs_name}")
@@ -32,6 +41,7 @@ if os.path.exists("test/test_template.py"):
 
 code_generator.code_gen(input=input_data, code_output_dir=f"src/{cs_name}", documentation_ouput_dir="docs")
 
-replace_string_in_file('pyproject.toml', 'ExampleCalculationService', cs_name)
-replace_string_in_file('Dockerfile', '<<INSERT_FOLDER_NAME>>', cs_name)
-replace_string_in_file('Dockerfile', '<<INSERT_IMPLEMENTATION_PYTHON_FILENAME>>', cs_python_name)
+replace_string_in_file('pyproject.toml', [FindReplace('ExampleCalculationService', cs_name)])
+replace_string_in_file(f'src/{cs_name}/{cs_python_name}.py', [FindReplace('CalculationServiceTest', cs_name), FindReplace('CalculationServiceTestBase', f'{cs_name}Base')])
+replace_string_in_file(f'test/test_{cs_python_name}.py', [FindReplace('CalculationServiceTest', cs_name), FindReplace('calculation_service_test', cs_python_name), FindReplace('ExampleCalculationService', cs_name)] )
+replace_string_in_file('Dockerfile', [FindReplace('<<INSERT_FOLDER_NAME>>', cs_name), FindReplace('<<INSERT_IMPLEMENTATION_PYTHON_FILENAME>>', cs_python_name)] )
