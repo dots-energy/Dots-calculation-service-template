f1fcd5a9855302f75961fa308acb0964679bf79b
busfahrerauftren <manavsharma375@gmail.com>
2026-07-20 15:05:45 +0200

    d

--- src/ExampleCalculationService/example_battery_service_dataclasses.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-@dataclass
-class MaxChargeAndDischargeOutput:
-    max_charge_active_power : float | None = None
-    max_discharge_active_power : float | None = None
